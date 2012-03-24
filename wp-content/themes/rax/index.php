401f4d1abb9a63daa2d663644d803d450327b4ef
John Heimkes IV <john@johnheimkes.com>
2012-03-24 11:47:53 -0500

    added loop into index.php

--- wp-content/themes/rax/index.php
+++ wp-content/themes/rax/index.php
@@ -1,7 +1,12 @@
-<?php
-get_header();
+<?php get_header(); ?>
 
-?>
-INDEX
-<?php
-get_footer();
+<?php $my_query = new WP_Query('post_type=page'); ?>
+
+<?php while ($my_query->have_posts()) : $my_query->the_post();
+	
+		the_title();
+		the_content();
+
+endwhile; ?>
+
+<?php get_footer(); ?>
