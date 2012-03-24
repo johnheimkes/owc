bc5792b7979619b9948921100650e1d579af61e8
John Heimkes IV <john@johnheimkes.com>
2012-03-24 16:46:35 -0500

    took out the read more link on the homepage static content

--- wp-content/themes/rax/home.php
+++ wp-content/themes/rax/home.php
@@ -10,7 +10,7 @@ Template Name: Homepage
  <div class="post">
   <h2 id="post-<?php the_ID(); ?>"><?php the_title();?></h2>
   <div class="entrytext">
-   <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+   <?php the_content(); ?>
   </div>
  </div>
  
