85886e84392bb2eb7f7a1c986466b040c8769e52
Merne William <mwilliam@nerdery.com>
2012-03-24 11:37:22 -0500

    Adding Family Stories Post Type.

--- wp-content/themes/rax/functions.php
+++ wp-content/themes/rax/functions.php
@@ -5,30 +5,58 @@ include 'static/Site.php';
 
 add_action( 'init', 'create_homepage_sliders' );
 function create_homepage_sliders() {
-	register_post_type( 'homepage_slider',
-		array(
-			'labels' => array(
-				'name' => __( 'Homepage Slides' ),
-				'singular_name' => __( 'Homepage Slide' ),
-				'add_new' => __( 'Add New' ),
-				'add_new_item' => __( 'Add New Homepage Slide' ),
-				'edit' => __( 'Edit' ),
+    register_post_type( 'homepage_slider',
+        array(
+            'labels' => array(
+                'name' => __( 'Homepage Slides' ),
+                'singular_name' => __( 'Homepage Slide' ),
+                'add_new' => __( 'Add New' ),
+                'add_new_item' => __( 'Add New Homepage Slide' ),
+                'edit' => __( 'Edit' ),
                 'edit_item' => __( 'Edit Homepage Slide' ),
                 'new_item' => __( 'New Homepage Slide' ),
-			),
-			'public' => true,
-			'supports' => array(
-            		'title',
-            		'author',
-            		'excerpt',
-            		'editor',
-            		'thumbnail',
-            		'revisions'
-            	),
-			'has_archive' => false,
-			'rewrite' => array('slug' => 'homepage_slider')
-		)
-	);
+            ),
+            'public' => true,
+            'supports' => array(
+                    'title',
+                    'author',
+                    'excerpt',
+                    'editor',
+                    'thumbnail',
+                    'revisions'
+                ),
+            'has_archive' => false,
+            'rewrite' => array('slug' => 'homepage_slider')
+        )
+    );
+}
+
+add_action( 'init', 'create_family_stories' );
+function create_family_stories() {
+    register_post_type( 'family_stories',
+        array(
+            'labels' => array(
+                'name' => __( 'Family Stories' ),
+                'singular_name' => __( 'Family Story' ),
+                'add_new' => __( 'Add New' ),
+                'add_new_item' => __( 'Add New Family Story' ),
+                'edit' => __( 'Edit' ),
+                'edit_item' => __( 'Edit Family Story' ),
+                'new_item' => __( 'New Family Story' ),
+            ),
+            'public' => true,
+            'supports' => array(
+                    'title',
+                    'author',
+                    'excerpt',
+                    'editor',
+                    'thumbnail',
+                    'revisions'
+                ),
+            'has_archive' => false,
+            'rewrite' => array('slug' => 'family_stories')
+        )
+    );
 }
 
 if (function_exists('add_theme_support')) {
