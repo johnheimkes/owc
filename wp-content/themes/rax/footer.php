f8187bd46b381ebf2a4ed1db1db53f33c2f8a73b
Matt Aho <maho@nerdery.com>
2012-03-25 00:41:13 -0500

    clearing floats on main divs

--- wp-content/themes/rax/footer.php
+++ wp-content/themes/rax/footer.php
@@ -1,6 +1,6 @@
         <div class="clear"></div>
     </section>
-    <footer id="site-footer">
+    <footer id="site-footer" class="group">
         <?php wp_nav_menu( array('menu' => 'Footer' )); ?>
     </footer>
     <?php wp_footer(); ?>
