0abf1ae9e84a9d9946061c9f03642521a02fa3fb
Ken Beck <beckkenneth@gmail.com>
2012-03-25 05:04:10 -0500

    Removed excerpts from Articles page.

--- wp-content/themes/rax/loops/loop-articles.php
+++ wp-content/themes/rax/loops/loop-articles.php
@@ -4,8 +4,5 @@
 <?php while ( $my_articles_query->have_posts() ) : $my_articles_query->the_post(); ?>
 <div class="post">
     <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-    <div class="entrytext">
-      <?php the_excerpt('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-    </div>
 </div>
 <?php endwhile; ?>
