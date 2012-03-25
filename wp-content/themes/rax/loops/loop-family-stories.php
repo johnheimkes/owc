8acdc8bef51cbfb1bb59c6bb89c351561b81a587
Merne William <mwilliam@nerdery.com>
2012-03-24 20:37:30 -0500

    Adding in single-post and editing other files for permalinks.

--- wp-content/themes/rax/loops/loop-family-stories.php
+++ wp-content/themes/rax/loops/loop-family-stories.php
@@ -1,10 +1,9 @@
 <?php $args = array( 'post_type' => 'family_stories', 'posts_per_page' => 10) ?>
 <?php $my_theme_query = new WP_Query( $args ); 
 
-while ( $my_theme_query->have_posts() ) : $my_theme_query->the_post();
-?>
+while ( $my_theme_query->have_posts() ) : $my_theme_query->the_post(); ?>
     <div class="post">
-         <h2 id="post-<?php the_ID(); ?>"><?php the_title();?></h2>
+         <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
             <?php if ( has_post_thumbnail()) : ?>
                 <?php the_post_thumbnail(); ?>
             <?php endif; ?>
@@ -12,6 +11,4 @@ while ( $my_theme_query->have_posts() ) : $my_theme_query->the_post();
           <?php the_excerpt('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
         </div>
     </div>
-<?php endwhile;
-// Reset Post Data
-wp_reset_postdata(); ?>
+<?php endwhile; ?>
--- wp-content/themes/rax/single-post.php
+++ wp-content/themes/rax/single-post.php
@@ -2,7 +2,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
  <div class="post">
-  <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+  <h2 id="post-<?php the_ID(); ?>"><?php the_title();?></h2>
   <div class="entrytext">
    <?php the_content(); ?>
   </div>
