7313e03b44d1ac4c4b713dc008f7c284981a3e5f
Merne William <mwilliam@nerdery.com>
2012-03-25 05:49:13 -0500

    bug fixes.

--- wp-content/themes/rax/header.php
+++ wp-content/themes/rax/header.php
@@ -5,7 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="MSSmartTagsPreventParsing" content="true" />
     <meta http-equiv="imagetoolbar" content="no" />
-    <title></title>
+    <title><?php
+    	/*
+    	 * Print the <title> tag based on what is being viewed.
+    	 */
+    	global $page, $paged;
+
+    	wp_title( '|', true, 'right' );
+
+    	// Add the blog name.
+    	bloginfo( 'name' );
+
+    	// Add the blog description for the home/front page.
+    	$site_description = get_bloginfo( 'description', 'display' );
+    	if ( $site_description && ( is_home() || is_front_page() ) )
+    		echo " | $site_description";
+
+    	// Add a page number if necessary:
+    	if ( $paged >= 2 || $page >= 2 )
+    		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+    	?></title>
     <script>
         var baseURL       = '<?php echo site_url(); ?>/';
         var ajaxURL       = '<?php echo admin_url(); ?>admin-ajax.php?action=';
