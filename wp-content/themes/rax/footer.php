10a6f679b788a839ca14638df182ab2f3d149e16
Merne William <mwilliam@nerdery.com>
2012-03-25 05:16:51 -0500

    Adding styles for stories.

--- wp-content/themes/rax/footer.php
+++ wp-content/themes/rax/footer.php
@@ -4,7 +4,8 @@
         <div class="sponsors">
             <a href="http://www.leavealegacy.org/" class="sponsor"><img src="<?php echo site_url(); ?>/wp-content/themes/rax/assets/images/leavealegacy.png" alt="" /></a>
             <a href="http://www.uwdp.org/" class="sponsor"><img src="<?php echo site_url(); ?>/wp-content/themes/rax/assets/images/unitedway.png" alt="" /></a>
-            <a href="https://www.facebook.com/groups/189064897805790/" class="sponsor"><img src="<?php echo site_url(); ?>/wp-content/themes/rax/assets/images/facebook.png" alt="" /></a>
+            <?php $fb_link = get_option('theme_option_facebook_prof'); ?>
+            <a href="<?php echo $fb_link; ?>" class="sponsor" target="_blank"><img src="<?php echo site_url(); ?>/wp-content/themes/rax/assets/images/facebook.png" alt="" /></a>
         </div>
         <div class="copyright">Copyright © 2005-<?php echo date('Y'); ?> Windmill Project - All Rights Reserved. The Windmill Project is a 501(c)(3) organization</div>
         <ul class="privacy-links">
