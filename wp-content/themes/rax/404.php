e7636f8e613898b3ba1c812fadce4e797513fa7e
Matt Aho <maho@nerdery.com>
2012-03-25 06:26:44 -0500

    added 404

---
+++ wp-content/themes/rax/404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+<div class="error-404">
+    <p>Sorry, we can't find the page you were looking for.</p>
+    
+</div>
+
+<?php get_footer(); ?>
